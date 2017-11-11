b1c81c98b09db25e5434275a51b1e367e571f0b8
berkerboy <archy@AdministorsAir2.local>
2017-11-11 18:45:16 -0500

    Implementation of Depth First Search

--- search/search.py
+++ search/search.py
@@ -86,8 +86,68 @@ def depthFirstSearch(problem):
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+
+    def retrace_path(parents, beginning, end):
+        """ Once we've found the reward, we need to regenerate the steps we took to
+        attain it. We can do this by traversing the graph from end to beginning, using
+        the dictionary of parents we compiled to do so.
+        """
+        steps = []
+
+        #  This inverts our directions, so our child --> parent steps become parent --> child steps
+        inverse_dir = dict(North='South',
+                           East='West',
+                           South='North',
+                           West='East')
+        s = beginning
+
+        while s != end:
+
+            # Specify the state we're trying to get to, which is the parent of the current one
+            target = parents[s]
+
+            # Add the (inverted) step to reach it
+            steps.extend([inverse_dir[d[1]] for d in problem.getSuccessors(s) if d[0] == target])
+
+            # Update the current step
+            s = target
+
+        # We now have an (inverted) list of directions from end -> beginning; reverse to get beginning -> end
+        steps.reverse()
+
+        return steps
+
+    S = util.Stack()
+
+    visited = util.Stack()  # Will store the visited states
+    parents = {}  # Will store the parent states of visited states
+
+    c = problem.getStartState()
+    S.push(c)  # Put our first state in the stack
+
+    #  Whilst we still have unvisited states
+    while S.isEmpty() is False:
+
+        # Mark the current state as visited
+        visited.push(c)
+
+        # If this is actually a goal state, we need to reconstruct the path and return it
+        if problem.isGoalState(c):
+            return retrace_path(parents, c, problem.getStartState())
+
+        # Put all of the adjacent states in the stack, if they're neww
+        for successor in problem.getSuccessors(c):
+            if successor[0] not in visited.list:
+                S.push(successor[0])
+                parents[successor[0]] = c
+
+        # And generate a new unexplored state
+        c = S.pop()
+
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
